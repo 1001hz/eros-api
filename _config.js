1c1a9d88475cf93c1b2f181c215e6f001c2038a0
1001hz <1001hz@gmail.com>
2018-01-03 05:15:01 +1100

    modify config

---
+++ _config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    secret: 'xcxcxc',
+    //database: 'mongodb://localhost:27017',
+    database: 'mongodb://1001hz:Hendri*@ds051720.mlab.com:51720/1001hz',
+    version: 'v1',
+    name: 'My App',
+    port: 3001,
+    host: 'http://localhost',
+    imagePath: './public/uploads/avatars/',
+    imageWebPath: '/uploads/avatars/',
+    tokenExpiryInMs: 3600000,
+    access: {
+        port: '4200',
+        host: 'http://localhost'
+    }
+};
